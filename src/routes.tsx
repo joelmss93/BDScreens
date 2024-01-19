239d51ec543917204056c7435444dc0d75c713ca
Joel Modesto <joelmss93@gmail.com>
2024-01-19 17:53:11 -0300

    chore: added list routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,13 +3,16 @@ import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { MovieInfo } from './pages/Movie'
 import { Layout } from './Layout'
+import { Lists } from './pages/Lists'
 
 export const AppRoutes: React.FC = () => {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<MovieInfo />} />
+        <Route path="/movies/:id" element={<MovieInfo />} />
+        <Route path="/movies/list/:prefix/:sortBy" element={<Lists />} />
+        <Route path="/series/list/:prefix/:sortBy" element={<Lists />} />
       </Routes>
     </Layout>
   )

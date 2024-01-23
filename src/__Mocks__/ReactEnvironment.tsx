2a2f6e47661eb315a7b1fd72e75412bcb4651fbe
Joel Modesto <joelmss93@gmail.com>
2024-01-23 17:16:16 -0300

    chore: mock folder structure change

--- src/__Mocks__/ReactEnvironment.tsx
+++ src/__Mocks__/ReactEnvironment.tsx
@@ -3,13 +3,16 @@ import { ThemeProvider } from 'styled-components'
 import dark from '../styles/theme/dark'
 import { queryClient } from '../service/queryClient'
 import { QueryClientProvider } from 'react-query'
+import { BrowserRouter } from 'react-router-dom'
 
 export const ReactTestEnvironment: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={dark}>{children}</ThemeProvider>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={dark}>{children}</ThemeProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   )
 }

7a1ad9ceb464719f23e6816082191e588d99811a
Joel Modesto <joelmss93@gmail.com>
2024-01-23 14:24:59 -0300

    chore: configs to envs & tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  // testIgnorePatterns: ['/node_modules/', '/.husky/', '/.vscode/'],
+  // testIgnorePatterns: ['/node_modules/'],
+  preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(jpg|jpeg|png|gif)$': 'jest-transform-stub',
   },
   testEnvironment: 'jsdom',
   resetMocks: true,
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'identity-obj-proxy',
+  },
 }

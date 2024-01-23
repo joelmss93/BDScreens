69f7a22b75ac9f638d4b8590cd0519f900154e3e
Joel Modesto <joelmss93@gmail.com>
2024-01-23 14:25:40 -0300

    chore: api using process env

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const apiKey = import.meta.env.VITE_MOVIE_API_KEY
+// const apiKey = import.meta.env.VITE_MOVIE_API_KEY
+const apiKey = process.env.VITE_MOVIE_API_KEY
 
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',

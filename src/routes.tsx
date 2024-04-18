fb29bd2b91fe21bc2c11b4f8e4dfbca533f8af5d
Joel Modesto <joelmss93@gmail.com>
2024-04-17 23:23:50 -0300

    chore: new routes to searchs

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { Home } from './pages/Home'
 import { MovieInfo } from './pages/Movie'
 import { Layout } from './Layout'
 import { Lists } from './pages/Lists'
+import { Search } from './pages/Search'
 
 export const AppRoutes: React.FC = () => {
   return (
@@ -11,8 +12,11 @@ export const AppRoutes: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies/:id" element={<MovieInfo />} />
+        <Route path="/series/:id" element={<MovieInfo />} />
         <Route path="/movies/list/:prefix/:sortBy" element={<Lists />} />
         <Route path="/series/list/:prefix/:sortBy" element={<Lists />} />
+        <Route path="/movies/search/:query" element={<Search />} />
+        <Route path="/series/search/:query" element={<Search />} />
       </Routes>
     </Layout>
   )
